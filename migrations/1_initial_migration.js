d545239e1131b68815984f43c65d7b65d56546d6
skari <skari@deakin.edu.au>
2020-01-15 15:01:30 +1100

    Created AssetManagement Contract

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 const Migrations = artifacts.require("Migrations");
 const Deadquiz = artifacts.require("Deadquiz");
+const AssetManagement = artifacts.require("AssetManagement")
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(Deadquiz);
+  deployer.deploy(AssetManagement);
 };
